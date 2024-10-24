b243dc954b10c989a5854a51012248983d89f1a7
MuhammadBurhan123 <burhantheschoolboy@gmail.com>
2024-10-24 07:26:41 +0500

    changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react';
-
 import App from './App';
+import { act } from 'react';  // Import act from react
 
 test('renders learn react link', () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
   const linkElement = screen.getByText(/Hello, Tailwind CSS with React!/i);
   expect(linkElement).toBeInTheDocument();
 });
